99a1f687014cc8159a656a16df30ef88665beda7
the_coding_bear <akanksha060801@gmail.com>
2024-04-30 23:40:24 +0530

    Smallest and Largest Element in array  is added

---
+++ src/ArrayQues/SmallOrLarge.java
@@ -0,0 +1,16 @@
+package ArrayQues;
+
+public class SmallOrLarge {
+    public static void main(String[] args) {
+        int arr[] = {12,34,45,56,78,9};
+        int largest = arr[0],smallest = arr[0];
+        for (int i=0;i<arr.length;i++){
+            if (smallest> arr[i])
+            smallest = arr[i];
+            if (largest<arr[i])
+                largest = arr[i];
+        }
+        System.out.println(smallest);
+        System.out.println(largest);
+    }
+}

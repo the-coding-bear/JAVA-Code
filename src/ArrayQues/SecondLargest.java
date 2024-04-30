6bf0159b3093faaff022eedbd269a844bdeaeed0
the_coding_bear <akanksha060801@gmail.com>
2024-04-30 23:59:40 +0530

    Second Smallest Element in array  is added

---
+++ src/ArrayQues/SecondLargest.java
@@ -0,0 +1,23 @@
+package ArrayQues;
+
+public class SecondLargest {
+    static int SecSmallest(int arr[],int n){
+        int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+        for (int i=0;i<n;i++){
+            if(arr[i]<first){
+                second=first;
+                first=arr[i];
+            } else if (second>arr[i]) {
+                second = arr[i];
+            }
+        }
+        return second;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {12,13,15,34,6};
+        int n = arr.length;
+        System.out.println(SecSmallest(arr,n));
+    }
+}

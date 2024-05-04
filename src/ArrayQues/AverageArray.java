19292da101298fb64b629e22c3b5778ff7ae15f6
the_coding_bear <akanksha060801@gmail.com>
2024-05-04 11:46:30 +0530

    Average of array is added

---
+++ src/ArrayQues/AverageArray.java
@@ -0,0 +1,14 @@
+package ArrayQues;
+
+public class AverageArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        int n = arr.length;
+        double sum =0;
+        for (int i = 0;i<n;i++){
+            sum+=(double)arr[i];
+        }
+        double average = sum/ n ;
+        System.out.println("The average is :" + average);
+    }
+}

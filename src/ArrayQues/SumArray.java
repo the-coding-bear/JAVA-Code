64cbef356677260b894444e742099389b202d7ca
the_coding_bear <akanksha060801@gmail.com>
2024-05-04 11:39:50 +0530

    Sum of element of array is added

---
+++ src/ArrayQues/SumArray.java
@@ -0,0 +1,13 @@
+package ArrayQues;
+
+public class SumArray {
+    public static void main(String[] args) {
+        int arr[] = {3,5,56,78,100};
+        int n = arr.length;
+        int sum = 0;
+        for (int i = 0;i<n;i++){
+            sum+=arr[i];
+        }
+        System.out.println("The sum of the element of the array is :" + sum);
+    }
+}

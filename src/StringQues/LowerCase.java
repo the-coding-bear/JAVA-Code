a7c3f0d1eab3c87986731b2315735caffb6914d1
Akanksha <akanksha060801@gmail.com>
2024-05-21 23:27:50 +0530

    Lower Case string is added.

---
+++ src/StringQues/LowerCase.java
@@ -0,0 +1,8 @@
+package StringQues;
+
+public class LowerCase {
+    public static void main(String[] args) {
+        String s = "GEEKSFORGEEKS";
+        System.out.println(s.toLowerCase());
+    }
+}

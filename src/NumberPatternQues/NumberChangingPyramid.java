28fec36f50f8456fe143f017728d5124ffbeea4b
Akanksha <akanksha060801@gmail.com>
2024-06-24 20:20:56 +0530

    Number changing pyramid pattern is formed

---
+++ src/NumberPatternQues/NumberChangingPyramid.java
@@ -0,0 +1,20 @@
+package NumberPatternQues;
+
+import java.util.Scanner;
+
+public class NumberChangingPyramid {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = read.nextInt();
+        int num=1;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(num);
+                System.out.print(" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}

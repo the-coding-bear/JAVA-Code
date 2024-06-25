e5cc4315daa898aed5f328b8625071e7b11c586b
Akanksha <akanksha060801@gmail.com>
2024-06-25 23:09:22 +0530

    check it is prime or not

---
+++ src/SimpleQue/PrimeOrNot.java
@@ -0,0 +1,27 @@
+package SimpleQue;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        if(isprime(num)){
+            System.out.println("prime number");
+        }else {
+            System.out.println("Not prime number");
+        }
+    }
+    public static boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        for (int i=2;i<n;i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}

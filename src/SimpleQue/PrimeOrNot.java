52909a5a8b463396af13814be21c3d2bac65156c
Akanksha <akanksha060801@gmail.com>
2024-06-26 09:11:45 +0530

    check it is prime or not

--- src/SimpleQue/PrimeOrNot.java
+++ src/SimpleQue/PrimeOrNot.java
@@ -17,7 +17,7 @@ public class PrimeOrNot {
         if(n<=1){
             return false;
         }
-        for (int i=2;i<n;i++){
+        for (int i=2;i<=Math.sqrt(n);i++){
             if (n%i==0){
                 return false;
             }

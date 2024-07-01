89bbd1121d6d1db54b50be65fc1cebe2369ae123
Akanksha <akanksha060801@gmail.com>
2024-07-01 22:51:30 +0530

    Power of a number using method

---
+++ src/SimpleQue/PowerOfNumberUsingMethod.java
@@ -0,0 +1,15 @@
+package SimpleQue;
+
+import java.util.Scanner;
+
+public class PowerOfNumberUsingMethod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number");
+        double n =sc.nextDouble();
+        System.out.print("Enter power of number");
+        double m =sc.nextDouble();
+        double reult = Math.pow(n,m);
+        System.out.println(reult);
+    }
+}

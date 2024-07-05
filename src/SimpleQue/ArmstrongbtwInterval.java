8935ab3f26f9e103c5f71e097d268d4e1c9da070
Akanksha <akanksha060801@gmail.com>
2024-07-05 23:49:07 +0530

    Find the intervals between two armstrong numbers

---
+++ src/SimpleQue/ArmstrongbtwInterval.java
@@ -0,0 +1,33 @@
+package SimpleQue;
+
+import java.util.Scanner;
+
+public class ArmstrongbtwInterval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter starting number");
+        int start = sc.nextInt();
+        System.out.print("Enter ending number");
+        int end = sc.nextInt();
+        System.out.print(printArmstrongNumbers(start,end));
+    }
+    public static boolean printArmstrongNumbers(int start, int end){
+        for (int i=start;i<=end;i++){
+            int sum =0;
+            int temp = i;
+            int digits = (int) Math.log10(i) + 1;
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += (int)Math.pow(digit, digits);
+                temp /= 10;
+            }
+
+            // if the sum is equal to the current number, print it
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+
+        return true;
+    }
+}

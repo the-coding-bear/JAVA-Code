629d79ac6192374d5bad7fe9e3c8e50e28716135
Akanksha <akanksha060801@gmail.com>
2024-07-07 23:35:56 +0530

    Change binary to decimal

--- src/SimpleQue/BinaryToDecimal.java
+++ src/SimpleQue/BinaryToDecimal.java
@@ -1,8 +1,12 @@
 package SimpleQue;
 
+import java.util.Scanner;
+
 public class BinaryToDecimal {
     public static void main(String[] args) {
-        String binary = "1101";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        String binary = sc.nextLine();
         int decimal = binaryToDecimal(binary);
         System.out.println("Decimal equivalent of binary number " + binary + " is: " + decimal);
     }

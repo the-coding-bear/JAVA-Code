8d98139dda98702098f77ddbdd1d651fbeabb404
Akanksha <akanksha060801@gmail.com>
2024-07-08 23:29:32 +0530

    check it is leap year or not

---
+++ src/SimpleQue/LeapYear.java
@@ -0,0 +1,31 @@
+package SimpleQue;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args){
+        // Scanner class for taking input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        // Initializing the answer variable
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}

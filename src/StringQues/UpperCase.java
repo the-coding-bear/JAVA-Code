24f89804d1b06de3dc08ad13d0308eda4fe28b16
Akanksha <akanksha060801@gmail.com>
2024-07-09 23:22:37 +0530

    convert to upper case

--- src/StringQues/UpperCase.java
+++ src/StringQues/UpperCase.java
@@ -1,8 +1,12 @@
 package StringQues;
 
+import java.util.Scanner;
+
 public class UpperCase {
     public static void main(String[] args) {
-        String s = "GEEKSFORGEEKS";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string");
+        String s = sc.nextLine();
         System.out.println(s.toUpperCase());
     }
 }

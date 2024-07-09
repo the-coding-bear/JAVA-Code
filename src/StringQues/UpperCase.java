e31e89ee26526564d9d8ac0a223d51cd4d02a9b4
Akanksha <akanksha060801@gmail.com>
2024-07-09 23:20:34 +0530

    convert to upper case

---
+++ src/StringQues/UpperCase.java
@@ -0,0 +1,8 @@
+package StringQues;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        String s = "GEEKSFORGEEKS";
+        System.out.println(s.toUpperCase());
+    }
+}

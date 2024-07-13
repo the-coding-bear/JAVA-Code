2a63c5b0c299e9a06d22b385a2c14ffede1804a4
Akanksha <akanksha060801@gmail.com>
2024-07-13 23:21:56 +0530

    cancatenate two name

---
+++ src/StringQues/Cancatenate.java
@@ -0,0 +1,9 @@
+package StringQues;
+
+public class Cancatenate {
+    public static void main(String[] args) {
+        String firstName = "well";
+        String lastName = "done!";
+        System.out.println(firstName.concat(lastName));
+    }
+}

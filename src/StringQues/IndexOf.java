9fda7b261d64f2240bd584d78ae031b3efa79667
Akanksha <akanksha060801@gmail.com>
2024-07-14 23:30:14 +0530

    occurence of the letter

---
+++ src/StringQues/IndexOf.java
@@ -0,0 +1,8 @@
+package StringQues;
+
+public class IndexOf {
+    public static void main(String[] args) {
+        String myStr = "Hello world by me";
+        System.out.println(myStr.indexOf("e", 5));
+    }
+}

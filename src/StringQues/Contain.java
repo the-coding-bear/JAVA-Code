fcc8b6b4facbc060f38d214c939546569c40d4f6
Akanksha <akanksha060801@gmail.com>
2024-07-17 23:39:52 +0530

    checking a string contains particular sequence

---
+++ src/StringQues/Contain.java
@@ -0,0 +1,11 @@
+package StringQues;
+
+import java.util.Scanner;
+
+public class Contain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.contains("aka"));
+    }
+}

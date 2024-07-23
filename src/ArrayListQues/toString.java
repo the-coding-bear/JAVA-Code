ff2b5c142aa87740595d7920a9c1f96f96013362
Akanksha <akanksha060801@gmail.com>
2024-07-23 22:57:42 +0530

    The toString method converts an array list to a string.

---
+++ src/ArrayListQues/toString.java
@@ -0,0 +1,14 @@
+package ArrayListQues;
+
+import java.util.ArrayList;
+
+public class toString {
+    public static void main(String[] args) {
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(45);
+        list.add(34);
+        String res = list.toString();
+        System.out.println(res);
+    }
+}

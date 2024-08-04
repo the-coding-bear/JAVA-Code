f1accbf8c2dae9488024b741f1c3660953615ccc
Akanksha <akanksha060801@gmail.com>
2024-08-04 23:07:58 +0530

    to replace the oldchar with newchar

---
+++ src/StringQues/Replace.java
@@ -0,0 +1,13 @@
+package StringQues;
+
+public class Replace {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        String newStr = str.replace('l', 'L');
+
+        System.out.println("Original string: " + str);
+        System.out.println("Replaced character: " + newStr);
+    }
+}

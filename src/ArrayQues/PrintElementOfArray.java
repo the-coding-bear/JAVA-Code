f054ff848806c48cedfec01fe8df4df0429b22ed
Akanksha <akanksha060801@gmail.com>
2024-08-11 00:00:11 +0530

    print element of array

---
+++ src/ArrayQues/PrintElementOfArray.java
@@ -0,0 +1,13 @@
+package ArrayQues;
+
+public class PrintElementOfArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        // Print each element of the array using a for loop
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.println(array[i]);
+        }
+    }
+}

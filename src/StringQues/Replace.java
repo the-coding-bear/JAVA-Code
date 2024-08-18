037ca717c30d08bb669310b902d3d6fb8e644d5b
Akanksha <akanksha060801@gmail.com>
2024-08-18 23:17:27 +0530

    Rotating an array

--- src/StringQues/Replace.java
+++ src/StringQues/Replace.java
@@ -1,11 +1,19 @@
 package StringQues;
 
+import java.util.Scanner;
+
 public class Replace {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String str = "Hello World";
 
-        String newStr = str.replace('l', 'L');
+
+        
+
+        System.out.println("Enter your string");
+        String str = sc.nextLine();
+
+        String newStr = str.replace('s', 'L');
 
         System.out.println("Original string: " + str);
         System.out.println("Replaced character: " + newStr);

d90ae6d4a176d02cfb4a4b962158f47195edf999
Akanksha <akanksha060801@gmail.com>
2024-09-01 21:41:53 +0530

    index of string

--- src/StringQues/IndexOf.java
+++ src/StringQues/IndexOf.java
@@ -2,7 +2,7 @@ package StringQues;
 
 public class IndexOf {
     public static void main(String[] args) {
-        String myStr = "Hello world by me";
+        String myStr = "Hello world this is Akanksha";
         System.out.println(myStr.indexOf("e", 5));
     }
 }

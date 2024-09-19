7357041152d9f7a5e0f61c539ec68aa92f3a899e
Akanksha <akanksha060801@gmail.com>
2024-09-19 22:24:21 +0530

    change number

--- src/ArrayQues/AverageArray.java
+++ src/ArrayQues/AverageArray.java
@@ -2,7 +2,7 @@ package ArrayQues;
 
 public class AverageArray {
     public static void main(String[] args) {
-        int arr[] = {108,56,3,456,67,6};
+        int arr[] = {108,56,43,456,67,6};
         int n = arr.length;
         double sum =0;
         for (int i = 0;i<n;i++){

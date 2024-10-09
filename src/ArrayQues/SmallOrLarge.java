01c4454fcecfac99b1cf0c9c981ade71f508ac37
Akanksha <akanksha060801@gmail.com>
2024-10-09 23:34:54 +0530

    change number

--- src/ArrayQues/SmallOrLarge.java
+++ src/ArrayQues/SmallOrLarge.java
@@ -2,11 +2,11 @@ package ArrayQues;
 
 public class SmallOrLarge {
     public static void main(String[] args) {
-        int arr[] = {12,34,45,56,78,9};
+        int[] arr = {12,34,45,56,78,9};
         int largest = arr[0],smallest = arr[0];
-        for (int i=0;i<arr.length;i++){
+        for(int i = 0; i< 6; i++){
             if (smallest> arr[i])
-            smallest = arr[i];
+              smallest = arr[i];
             if (largest<arr[i])
                 largest = arr[i];
         }

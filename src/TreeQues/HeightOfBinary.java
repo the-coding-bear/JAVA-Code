187c916b6923505191719694fa7e7add548b0ee6
Akanksha <akanksha060801@gmail.com>
2024-10-24 23:24:53 +0530

    check the height of binary tree

--- src/TreeQues/HeightOfBinary.java
+++ src/TreeQues/HeightOfBinary.java
@@ -42,7 +42,7 @@ public class HeightOfBinary {
             root.left.right.left.left.left = new Node(9);
             root.left.right.left.left.right = new Node(11);
             root.left.right.left.left.right.left = new Node(12);
-            root.left.right.left.left.right.right = new Node(13);
+            root.left.right.left.left.right.right = new Node(14);
 
 
 

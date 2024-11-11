98f1e0b9d7c0e60199f58907c4296f208f5c14c1
Akanksha <akanksha060801@gmail.com>
2024-11-11 23:10:07 +0530

    check that two strings are anagram or not.

--- src/ArrayQues/Anagrams.java
+++ src/ArrayQues/Anagrams.java
@@ -17,7 +17,7 @@ public class Anagrams {
 
         public static void main(String[] args) {
             String s1 = "akanksh";
-            String s2 = "ankasksh";
+            String s2 = "ankasksha";
             System.out.println(areAnagrams(s1, s2));
         }
 
